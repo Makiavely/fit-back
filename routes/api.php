cca94e69c988edbbc9c40a9a2d36e8c81735ef6f
Makiavely <fiolemail@gmail.com>
2021-12-05 06:00:45 +0300

    11b Middlewares_ We protect route users with auth:api middlewares now we need to be authorise and input token to request and only in that case we can take this route

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// Route::get('hello', function(){
-//     return 'Hello';
-// });
-
-
-// Route::get('hello', 'App\Http\Controllers\UserController@index');
-
-// Route::get('users', 'App\Http\Controllers\UserController@index');
-
-// Route::apiResource('users', 'App\Http\Controllers\UserController');
-
-Route::apiResource('users', UserController::class);
 Route::post('login', 'App\Http\Controllers\AuthController@login');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('users', UserController::class);
+});
