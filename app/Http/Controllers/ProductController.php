df59f839a2a70126ec56b5a52d6a86690eaa3ef6
Makiavely <fiolemail@gmail.com>
2021-12-21 17:21:39 +0300

    19b Images_Made method in ProductController for store Image, Change path for storing image in filesystems.php and .env

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -22,7 +24,21 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
+        $file = $request->file('image');
+        $name = Str::random(10);
+        $url = \Storage::putFileAs('images', $file, $name . '.' . $file->extension());
 
+        $product = Product::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            /*'image' => $url,*/
+            /*'image' => 'http://localhost:8000/images/' . $url,*/
+            /*'image' => env('APP_URL') .  '/images/' . $url,*/
+            'image' => env('APP_URL') .  '/' . $url,
+            'price' => $request->input('price'),
+        ]);
+
+        return response($product, Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
