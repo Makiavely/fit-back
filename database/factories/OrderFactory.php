5813ca9af3169653bccbcacd4f043d924b8de404
Makiavely <fiolemail@gmail.com>
2021-12-22 12:31:26 +0300

    21b Orders_Create orders_table and order_items_table Then create Order and OrderItem model Then create OrderFactory and OrderItemFactory and OrderSeeder Then seeding database

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            "first_name" => $this->faker->firstName(),
+            "last_name" => $this->faker->lastName(),
+            "email" => $this->faker->email,
+        ];
+    }
+}
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         Product::factory()->times(30)->create();
